331e3197316a842c9aead7694e68d7eab40c4078
Sasha <shurunok0@gmail.com>
2023-05-30 15:00:59 +0300

    Добавьте файлы проекта.

---
+++ lb8/MyForm.cpp
@@ -0,0 +1,44 @@
+﻿#include "MyForm.h"
+
+private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	string input;
+	cout << "Ââåä³òü ðÿäîê: ";
+	getline(cin, input);
+
+	// Ïåðåòâîðåííÿ ìàëèõ ë³òåð íà âåëèê³
+	for (char& c : input) {
+		if (islower(c)) {
+			c = toupper(c);
+		}
+	}
+
+	cout << "Ïåðåòâîðåíèé ðÿäîê: " << input << endl;
+
+	return 0;
+}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		string fullName;
+		cout << "
+		getline(cin, fullName);
+		size_t lastNameLength = fullName.find(' ');
+		size_t firstNameLength = fullName.find(' ', lastNameLength + 1) - lastNameLength - 1;
+		size_t middleNameLength = fullName.length() - lastNameLength - firstNameLength - 2;
+
+		
+		size_t totalLength = fullName.length() - 2;
+
+		cout << "Äîâæèíà ïð³çâèùà: " << lastNameLength << endl;
+		cout << "Äîâæèíà ³ìåí³: " << firstNameLength << endl;
+		cout << "Äîâæèíà ïî áàòüêîâ³: " << middleNameLength << endl;
+		cout << "Äîâæèíà ðÿäêà, ùî îá'ºäíóº âñ³ ñëîâà: " << totalLength << endl;
+
+		
+		string lastName = fullName.substr(0, lastNameLength);
+		string firstName = fullName.substr(lastNameLength + 1, firstNameLength);
+		string middleName = fullName.substr(lastNameLength + 2 + firstNameLength, middleNameLength);
+		cout << "Ïð³çâèùå: " << lastName << endl;
+		cout << "²í³ö³àëè: " << lastName[0] << "." << firstName[0] << "." << middleName[0] << "." << endl;
+
+		return 0;
+	}
+	}
---
+++ lb8/MyForm.h
@@ -0,0 +1,105 @@
+#pragma once
+
+namespace lb8 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
+	private: System::Windows::Forms::CheckBox^ checkBox1;
+	private: System::DirectoryServices::DirectoryEntry^ directoryEntry1;
+	private: System::Windows::Forms::ComboBox^ comboBox1;
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+			this->directoryEntry1 = (gcnew System::DirectoryServices::DirectoryEntry());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(157, 249);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->Location = System::Drawing::Point(408, 223);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(95, 20);
+			this->checkBox1->TabIndex = 1;
+			this->checkBox1->Text = L"checkBox1";
+			this->checkBox1->UseVisualStyleBackColor = true;
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Location = System::Drawing::Point(0, 0);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(121, 24);
+			this->comboBox1->TabIndex = 2;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(604, 450);
+			this->Controls->Add(this->comboBox1);
+			this->Controls->Add(this->checkBox1);
+			this->Controls->Add(this->button1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	};
+}
